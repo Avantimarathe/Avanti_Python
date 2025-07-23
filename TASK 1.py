3858f748bb2c295f75610b0710ecdd578e2e8ce3
Avantimarathe <avantimarathe2508@gmail.com>
2025-07-23 13:02:25 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first number:  ")
+b=input("Enter the second number:  ")
+a=int(a)
+b=int(b)
+add=a + b
+print(f"Addition: {add}")
+sub=a - b
+print(f"Substraction: {sub}")
+mul=a * b
+print(f"Multiplication: {mul}")
+div=a/b
+print(f"Division: {div}")
