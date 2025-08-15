bf5c6fd7f4c02e1bf762a712a684f89d0fb1082c
Avantimarathe <avantimarathe2508@gmail.com>
2025-08-15 11:05:27 +0530

    Add files via upload

---
+++ Assignment 4, Task 2.py
@@ -0,0 +1,21 @@
+
+user_input = input("Enter some text to write to the file: ")
+
+with open("output.txt", "w") as file:
+    file.write(user_input + "\n")
+
+print("Data written to output.txt.")
+
+
+additional_input = input("Enter more text to append to the file: ")
+
+with open("output.txt", "a") as file:
+    file.write(additional_input + "\n")
+
+print("Additional data appended to output.txt.")
+
+
+print("\nFinal contents of output.txt:")
+with open("output.txt", "r") as file:
+    content = file.read()
+    print(content)
